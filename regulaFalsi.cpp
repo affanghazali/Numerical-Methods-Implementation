2e2430f4a7e230dd2980a4a941cfd9d7ba396a74
Ammar Ghazali <ammarmghazali@gmal.com>
2020-06-03 23:12:56 +0500

    Third Commit

--- regulaFalsi.cpp
+++ regulaFalsi.cpp
@@ -1,16 +1,15 @@
 #include<bits/stdc++.h>
 #include<iostream>
-#include<cmath>
 #include<math.h>
+#include<cmath>
 
 using namespace std;
-
-float func(float);
+float equation(float);
 
 int main(){
 	float a, b, z, c, m, n;
 	for(int i=0;i<100;i++){
-		z = func(i);
+		z = equation(i);
 		if(z >= 0){
 			b = i;
 			a = --i;
@@ -20,29 +19,26 @@ int main(){
 	start:
 		cout<<"First Initial Point => "<<a;
 		cout<<"\nSecond Initial Point => "<<b<<"\n\n";
-		
 		for(int i=0;i<100;i++){
 			float h, d;
-			m = func(a);
-			n = func(b);
-			
+			m = equation(a);
+			n = equation(b);
 			c = ((a * n) - (b * m)) / (n - m);
-			a = b;
-			b = c;
-			
-			z = func(c);
-			if(z > 0 && z < 0.09){		//Stoping Criteria
-				goto end;
+        	a = c;
+        	z = equation(c);
+        	if(z > 0 && z < 0.09){
+        		goto end;
 			}
 			else{
 				cout<<"Iteration "<<i<<" : z = "<<z<<"\n";
 			}
 		}
-	end:
-		cout<<"\n\nRoot => "<<z;
-		return 0;
+		end:
+			cout<<"Root => "<<z;
+			return 0;
 }
-float func(float x)
-{
-	return (pow(x,2)-(2*x)+4); // original equation
+
+float equation(float x) {
+    return (pow(x, 7) - (10 * pow(x, 3)) - 9);  // origial equation
 }
+
